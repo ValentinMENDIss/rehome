6cd335908a44c46360a0063459a88213242e6b53
ValentinMENDIss <valiksuper58@gmail.com>
2024-09-07 20:54:31 +0200

    Updated pull feature

--- src/rehome-gum.sh
+++ src/rehome-gum.sh
@@ -13,10 +13,10 @@ CONF_DIRPATH=~/.config/rehome.conf
 DIRPATH=~/.config
 OPTION=$(gum choose "push" "pull")
 
-# ADD PATHS OF PROGRAMS THAT YOU WANT TO COPY TO CONFIG FILE
+# ADD PATHS OF PROGRAMS THAT YOU WANT TO COPY TO CONFIG FILE AND ADD IT TO INPUT FILES
 FILE1=~/.config/bspwm/bspwmrc
 FILE2=~/.config/sxhkd/sxhkdrc
-
+INPUT_FILES=("$FILE1" "$FILE2")
 
 # check if config file is created at startup
 if [ ! -d $CONF_DIRPATH ]; then
@@ -33,6 +33,15 @@ if [ "$OPTION" == "push" ]; then
         echo -e "\npush function is under construction..."
 elif [ "$OPTION" == "pull" ]; then
         echo -e "\npull function is under construction..."
-        cat "$FILE1" "$FILE2" > "$CONF_DIRPATH"
+        > "$CONF_DIRPATH"   # empty the rehome config file 
+        for file in "${INPUT_FILES[@]}"; do
+                {
+                        echo "----- Start of $file -----"
+                        cat "$file"
+                        echo "----- End of $file -----"
+                } >>"$CONF_DIRPATH"
+        done
+        
+        echo "ReHome Configuration file was successfully generated"
         wc -l "$CONF_DIRPATH"
 fi
